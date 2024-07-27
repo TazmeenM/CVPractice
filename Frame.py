4ec4da47a2b94b99527c1737fd678360b24085d4
TazmeenM <tazmeen.mohsin@gmail.com>
2024-07-27 19:28:33 -0400

    Add files via upload

---
+++ Frame.py
@@ -0,0 +1,29 @@
+import pygame
+import cv2
+import time
+import cvPractice
+
+#Declaring and initializing variables relating to the screen
+pygame.init()
+displayX = int(1280)
+displayY = int(720)
+window = pygame.display.set_mode((displayX, displayY))
+screen = pygame.display.get_surface()
+running = True
+screenShown = str()
+k = cv2.waitKey(5)
+def loadingImages(image):
+    image = pygame.image.load(image)
+    return image
+
+background = loadingImages("Desktop\Tazmeen\CVPractice\\blueBackground.png")
+
+def gameLoop():
+    while(k != 27):
+        screen.blit(background, (0, 0))
+        pygame.display.update()
+        cvPractice.what_to_do_function()
+    
+pygame.display.set_caption("Pygame")
+
+gameLoop()
--- cvPractice.py
+++ cvPractice.py
@@ -1,3 +1,4 @@
+#This version doesn't work; some errors still need to be fixed
 import cv2
 import numpy
 
@@ -22,88 +23,106 @@ def fraction_of_frame(number_of_pixels):
     total_number_of_pixels = resolution[0] * resolution[1]
     return (number_of_pixels / total_number_of_pixels)
 
-while (what_to_do == 0):
-    
-    _, frame = cap.read()
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, lower_colour_boundary, upper_colour_boundary)
+def what_to_do_function():
+    what_to_do = 0
 
-    red_counter = 0
-    yellow_counter = 0
-    blue_counter = 0
-    red_mask = cv2.inRange(hsv, lower_red, upper_red)
-    yellow_mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
-    blue_mask = cv2.inRange(hsv, lower_blue, upper_blue)
-    k = cv2.waitKey(5)
+    lower_colour_boundary = numpy.array([0, 100, 100])
+    upper_colour_boundary = numpy.array([43, 255, 255])
 
-    if (k == 27):
-        break
+    lower_red = numpy.array([0, 100, 100])
+    upper_red = numpy.array([43, 255, 255])
 
-    cv2.imshow("Window", mask)
-    for i in red_mask:
-        if (red_mask[i].any()):
-            red_counter += 1
+    lower_yellow = numpy.array([43, 100, 100])
+    upper_yellow = numpy.array([86, 255, 255])
 
-    for i in yellow_mask:
-        if (yellow_mask[i].any()):
-            yellow_counter += 1
-    
-    for i in blue_mask:
-        if (blue_mask[i].any()):
-            blue_counter += 1
+    lower_blue = numpy.array([86, 100, 100])
+    upper_blue = numpy.array([129, 255, 255])
 
-    if (red_counter == yellow_counter == blue_counter):
-        print("all")
-        break
-    elif (red_counter > yellow_counter):
-        if (red_counter > blue_counter):
-            print("red")
-            cv2.imshow("Red", red_mask)
-        elif (blue_counter > red_counter):
-            print("blue")
-            cv2.imshow("Blue", blue_mask)
+    cap = cv2.VideoCapture(0)
+    while (what_to_do == 0):
+        
+        _, frame = cap.read()
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, lower_colour_boundary, upper_colour_boundary)
+
+        red_counter = 0
+        yellow_counter = 0
+        blue_counter = 0
+        red_mask = cv2.inRange(hsv, lower_red, upper_red)
+        yellow_mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+        blue_mask = cv2.inRange(hsv, lower_blue, upper_blue)
+        k = cv2.waitKey(5)
+
+        if (k == 27):
+            break
+
+        cv2.imshow("Window", mask)
+        for i in red_mask:
+            if (red_mask[i].any()):
+                red_counter += 1
+
+        for i in yellow_mask:
+            if (yellow_mask[i].any()):
+                yellow_counter += 1
+        
+        for i in blue_mask:
+            if (blue_mask[i].any()):
+                blue_counter += 1
+
+        if (red_counter == yellow_counter == blue_counter):
+            print("all")
+            break
+        elif (red_counter > yellow_counter):
+            if (red_counter > blue_counter):
+                print("red")
+                cv2.imshow("Red", red_mask)
+            elif (blue_counter > red_counter):
+                print("blue")
+                cv2.imshow("Blue", blue_mask)
+            else:
+                print("red and blue")
+                cv2.imshow("Red", red_mask)
+                cv2.imshow("Blue", blue_mask)
+        elif (yellow_counter > red_counter):
+            if (yellow_counter > blue_counter):
+                print("yellow")
+                cv2.imshow("Yellow", yellow_mask)
+            elif (blue_counter > yellow_counter):
+                print("blue")
+                cv2.imshow("Blue", blue_mask)
+            else:
+                print("yellow and blue")
+                cv2.imshow("Yellow", yellow_mask)
+                cv2.imshow("Blue", blue_mask)
         else:
-            print("red and blue")
-            cv2.imshow("Red", red_mask)
-            cv2.imshow("Blue", blue_mask)
-    elif (yellow_counter > red_counter):
-        if (yellow_counter > blue_counter):
-            print("yellow")
-            cv2.imshow("Yellow", yellow_mask)
-        elif (blue_counter > yellow_counter):
             print("blue")
-            cv2.imshow("Blue", blue_mask)
+        
+        if (fraction_of_frame(red_counter) > 0.001):
+            print("Too much red")
+            break
         else:
-            print("yellow and blue")
-            cv2.imshow("Yellow", yellow_mask)
-            cv2.imshow("Blue", blue_mask)
-    else:
-        print("blue")
-    
-    if (fraction_of_frame(red_counter) > 0.001):
-        print("Too much red")
-        break
-    else:
-        print("Not enough red")
+            print("Not enough red")
 
 
-while (what_to_do == 1):
-    _, frame = cap.read()
-    lower_colour_boundary = numpy.array([0, 25, 25])
-    upper_colour_boundary = numpy.array([255, 230, 230])
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, lower_colour_boundary, upper_colour_boundary)
-    k = cv2.waitKey(5)
+    while (what_to_do == 1):
+        _, frame = cap.read()
+        lower_colour_boundary = numpy.array([0, 25, 25])
+        upper_colour_boundary = numpy.array([255, 230, 230])
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, lower_colour_boundary, upper_colour_boundary)
+        k = cv2.waitKey(5)
 
-    if (k == 27):
-        break
-    '''
-    cv2.imshow("Window", mask)
-    threshold = cv2.threshold(mask, 100, 255, 0)
-    print("before")
-    contours = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    print("after")
-    contours_drawing = cv2.drawContours(mask, contours, -1, (10, 10, 10), 3)
-    cv2.imshow("Contours", contours_drawing)
-    '''
+        if (k == 27):
+            break
+        '''
+        cv2.imshow("Window", mask)
+        threshold = cv2.threshold(mask, 100, 255, 0)
+        print("before")
+        contours = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        print("after")
+        contours_drawing = cv2.drawContours(mask, contours, -1, (10, 10, 10), 3)
+        cv2.imshow("Contours", contours_drawing)
+        '''
+
+what_to_do_function()
     
